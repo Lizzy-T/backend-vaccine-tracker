410ea5f04f7d2502a842f0f6bdb62c8077103966
github username <github email address>
2019-12-02 16:10:51 -0700

    my_vaccines table, routes index & create, index.html.erb updated

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,5 @@
 class User < ApplicationRecord
     has_secure_password
+    has_many :my_vaccines
+    has_many :vaccines, through: :my_vaccines
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
-  resources :users, only: [:index, :show, :create, :update, :destroy]
+
+  resources :users, only: [:index, :show, :create, :update, :destroy] 
+  
+  resources :users, only: [:show] do
+    resources :my_vaccines, only: [:index, :create]
+  end
+
   resources :vaccines, only: [:index, :show]
   post '/login', to: 'auth#create'
   root "welcome#index"
